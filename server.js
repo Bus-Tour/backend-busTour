500fb9318b8d36c78df8542a6be82e6dc0a8a388
ahmadalasaad <ahmadalserese9754@gnail.com>
2021-09-20 12:26:09 +0300

    backend user

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const express = requier('express');
+const express = require('express');
 const app=express();
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -23,41 +23,43 @@ const MONGO_URL = process.env.MONGO_URL;
 const DB_NAME = process.env.DB_NAME;
 
 mongoose.connect(`${MONGO_URL}/${DB_NAME}`);
-
+////dataseed
+const seedUser = require('./helper/userSeed.seed.js');
+// seedUser();
 // admin get,create,update,delete data
-app.get('/admin',getAdmin);
-app.post('/admin',createAdmin);
-app.update('/admin',updateAdmin);
-app.delete('/admin',deleteAdmin);
+// app.get('/admin',getAdmin);
+// app.post('/admin',createAdmin);
+// app.put('/admin',updateAdmin);
+// app.delete('/admin',deleteAdmin);
 
 // Bus Info  get,create,update,delete data
-app.get('/busInfo',getBusInfo);
-app.post('/busInfo',createBusInfo);
-app.update('/busInfo/:busInfo_id',updateBusInfo);
-app.delete('/busInfo/:busInfo_id',deleteBusInfo);
+// app.get('/busInfo',getBusInfo);
+// app.post('/busInfo',createBusInfo);
+// app.put('/busInfo/:busInfo_id',updateBusInfo);
+// app.delete('/busInfo/:busInfo_id',deleteBusInfo);
 
 // Students get,create,update,delete data
-app.get('/students',getStudents);
-app.post('/students',createStudents);
-app.update('/students/:students_id',updateStudents);
-app.delete('/students/:students_id',deleteStudents);
+// app.get('/students',getStudents);
+// app.post('/students',createStudents);
+// app.put('/students/:students_id',updateStudents);
+// app.delete('/students/:students_id',deleteStudents);
 
 // Teacher get,create,update,delete data
-app.get('/teacher',getTeacher);
-app.post('/teacher',createTeacher);
-app.update('/teacher/:teacher_id',updateTeacher);
-app.delete('/teacher/:teacher_id',deleteTeacher);
+// app.get('/teacher',getTeacher);
+// app.post('/teacher',createTeacher);
+// app.put('/teacher/:teacher_id',updateTeacher);
+// app.delete('/teacher/:teacher_id',deleteTeacher);
 
 // User get,create,update,delete data
 app.get('/user',getUser);
 app.post('/user',createUser);
-app.update('/user/:user_id',updateUser);
+app.put('/user/:user_id',updateUser);
 app.delete('/user/:user_id',deleteUser);
 
 // Parents interface get data
-app.get('/parentsInterFace',getParentsInterFace);
+// app.get('/parentsInterFace',getParentsInterFace);
 
 // Teacher interface get data
-app.get('/teacherInterFace',getTeacherInterFace);
+// app.get('/teacherInterFace',getTeacherInterFace);
 
 app.listen(PORT);
